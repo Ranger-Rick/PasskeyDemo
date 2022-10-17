b467ab1f44ae72959e9be04eb4ce7e1086a39f13
Rick Bordelon <Rick.Bordelon@onshoreoutsourcing.com>
2022-10-17 09:38:22 -0500

    Removed unused components and added circle-color component

--- PasskeyDemo/ClientApp/src/app/app.module.ts
+++ PasskeyDemo/ClientApp/src/app/app.module.ts
@@ -5,22 +5,20 @@ import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { CircleColorComponent } from './circle-color/circle-color.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    CircleColorComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -28,9 +26,7 @@ import { RegisterComponent } from './register/register.component';
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: LoginComponent, pathMatch: 'full' },
-      { path: 'register', component: RegisterComponent },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
+      { path: 'register', component: RegisterComponent }
     ])
   ],
   providers: [],
---
+++ PasskeyDemo/ClientApp/src/app/circle-color/circle-color.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-circle-color',
+  templateUrl: './circle-color.component.html',
+  styleUrls: ['./circle-color.component.css']
+})
+export class CircleColorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
